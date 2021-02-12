15f931467326fecfb0d5829b5df1a27a230c4ba1
paciFIST-Studios <ellie@pacifist.studio>
2021-02-12 01:48:00 -0800

    Add SquareGrid and tests

---
+++ Assets/Scripts/SquareGrid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareGrid
+{
+    public static readonly int MAX_SIDE = 30000;
+
+    private int width;
+    private int height;
+
+    private int[,] gridArray;
+
+
+    public int Width  { get { return this.width; } }
+    public int Height { get { return this.height; } }
+
+
+    private int ClampToValidRange(int value)
+    {
+        if (value > MAX_SIDE)
+        {
+            return MAX_SIDE;
+        }
+        else if (value < 0)
+        {
+            return 0;
+        }
+
+        return value;
+    }
+
+
+    public SquareGrid(int width, int height)
+    {
+        this.width  = ClampToValidRange(width);
+        this.height = ClampToValidRange(height);
+
+
+        gridArray = new int[this.width, this.height];
+    }
+
+}
---
+++ Assets/Tests/SquareGrid_test.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SquareGrid_test
+    {
+        [Test]
+        public void SquareGrid_IsNotConstructible_AtSizeZeroZero()
+        {
+            var sg = new SquareGrid(0, 0);
+            Assert.AreEqual(0, sg.Width);
+            Assert.AreEqual(0, sg.Height);
+        }
+
+        [Test]
+        public void SquareGrid_IsConstructible_AtSizeOneOne()
+        {
+            var sg = new SquareGrid(1, 1);
+            Assert.AreEqual(1, sg.Width);
+            Assert.AreEqual(1, sg.Height);
+        }
+
+        [Test]
+        public void SquareGrid_IsNotConstructible_AtSizeMinusOneMinusOne()
+        {
+            var sg = new SquareGrid(-1, -1);
+            Assert.AreEqual(0, sg.Width);
+            Assert.AreEqual(0, sg.Height);
+        }
+
+        [Test]
+        public void SquareGrid_IsConstructible_AtSizeMaxSide()
+        {
+            var sg = new SquareGrid(SquareGrid.MAX_SIDE, SquareGrid.MAX_SIDE);
+            Assert.AreEqual(SquareGrid.MAX_SIDE, sg.Width);
+            Assert.AreEqual(SquareGrid.MAX_SIDE, sg.Height);
+        }
+
+        [Test]
+        public void SquareGrid_IsNotConstructible_AtSizeGreaterThanMaxSide()
+        {
+            var sg = new SquareGrid(SquareGrid.MAX_SIDE + 1, SquareGrid.MAX_SIDE + 1);
+            Assert.AreEqual(SquareGrid.MAX_SIDE, sg.Width);
+            Assert.AreEqual(SquareGrid.MAX_SIDE, sg.Height);
+        }
+
+
+    }
+}
